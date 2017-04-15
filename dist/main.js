983ef908434fe8a6d1212154e09fb13c22ee512a
Axel <axelfuh@gmail.com>
2017-04-15 11:08:02 +0200

    Add navigation dots

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,99 @@
-"use strict";function _toConsumableArray(e){if(Array.isArray(e)){for(var r=0,t=Array(e.length);r<e.length;r++)t[r]=e[r];return t}return Array.from(e)}var app=document.getElementById("app"),slider=app.querySelector(".slider"),next=app.querySelector(".next"),prev=app.querySelector(".prev"),pics=[],images=20,count=0,addImages=function(){for(var e=0;e<images;e++){var r='<div class="pics">\n          <img src="https://source.unsplash.com/random?sig='+e+'" alt="pic'+(e+1)+'/1980x1080">\n        </div>';slider.insertAdjacentHTML("beforeend",r)}pics.push.apply(pics,_toConsumableArray(app.querySelectorAll(".pics")))},slide={next:function(){count===images-1?(count=0,pics.forEach(function(e){return e.style.transform="translateX(0)"})):(count++,pics.forEach(function(e){return e.style.transform="translateX("+-100*count+"%)"}))},prev:function(){0===count?(count=images-1,pics.forEach(function(e){return e.style.transform="translateX("+-100*(images-1)+"%)"})):(count--,pics.forEach(function(e){return e.style.transform="translateX("+-100*count+"%)"}))}};next.addEventListener("click",slide.next),prev.addEventListener("click",slide.prev);var interval=setInterval(slide.next,1e4);window.onload=addImages;
+"use strict";
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var app = document.getElementById("app");
+var slider = app.querySelector(".slider");
+var next = app.querySelector(".next");
+var prev = app.querySelector(".prev");
+var dots = app.querySelector(".dots");
+var dot = [];
+var pics = [];
+var images = 6;
+var count = 0;
+
+var render = {
+  addImages: function addImages() {
+    for (var i = 0; i < images; i++) {
+      var _render = "<div class=\"pics fl\">\n            <img src=\"https://source.unsplash.com/random?sig=" + i + "\" alt=\"pic" + (i + 1) + "/900x500\">\n          </div>";
+      slider.insertAdjacentHTML("beforeend", _render);
+    }
+    pics.push.apply(pics, _toConsumableArray(app.querySelectorAll(".pics")));
+  },
+  addDots: function addDots() {
+    for (var i = 0; i < images; i++) {
+      var _render2 = "<div class=\"dot\" data-pos=\"" + i + "\">\n            <i class=\"fa fa-stop\" aria-hidden=\"true\"></i>\n          </div>";
+      dots.insertAdjacentHTML("beforeend", _render2);
+    }
+    dot.push.apply(dot, _toConsumableArray(app.querySelectorAll(".dot")));
+    dot.forEach(function (e) {
+      return e.addEventListener("click", setDot.select);
+    });
+  }
+};
+
+var slide = {
+  next: function next() {
+    if (count === images - 1) {
+      count = 0;
+      pics.forEach(function (i) {
+        return i.style.transform = "translateX(" + count + ")";
+      });
+    } else {
+      count++;
+      pics.forEach(function (i) {
+        return i.style.transform = "translateX(" + -100 * count + "%)";
+      });
+    }
+    setDot.paint();
+  },
+  prev: function prev() {
+    if (count === 0) {
+      count = images - 1;
+      pics.forEach(function (i) {
+        return i.style.transform = "translateX(" + -100 * (images - 1) + "%)";
+      });
+    } else {
+      count--;
+      pics.forEach(function (i) {
+        return i.style.transform = "translateX(" + -100 * count + "%)";
+      });
+    }
+    setDot.paint();
+  },
+  jump: function jump(val) {
+    pics.forEach(function (i) {
+      return i.style.transform = "translateX(" + -100 * val + "%)";
+    });
+    count = val;
+  }
+};
+
+var setDot = {
+  select: function select() {
+    var val = this.attributes["data-pos"].value;
+    slide.jump(val);
+    setDot.paint();
+  },
+  paint: function paint() {
+    var on = app.querySelector(".dot[data-pos=\"" + count + "\"]");
+    var off = app.querySelectorAll(".dot:not([data-pos=\"" + count + "\"])");
+    on.classList.add("dot-on");
+    off.forEach(function (i) {
+      return i.classList.remove("dot-on");
+    });
+  }
+};
+
+var init = function init() {
+  render.addImages();
+  render.addDots();
+  setDot.paint();
+};
+
+next.addEventListener('click', slide.next);
+prev.addEventListener('click', slide.prev);
+
+// const interval = setInterval(slide.next, 10000);
+
+window.onload = init;
--- main.js
+++ main.js
@@ -2,32 +2,48 @@ const app = document.getElementById("app");
 const slider = app.querySelector(".slider");
 const next = app.querySelector(".next");
 const prev = app.querySelector(".prev");
+const dots = app.querySelector(".dots");
+const dot = [];
 const pics = [];
-const images = 20;
+const images = 6;
 let count = 0;
 
-const addImages = () => {
-      for (let i = 0; i < images; i++) {
-        const render =
-        `<div class="pics">
-          <img src="https://source.unsplash.com/random?sig=${i}" alt="pic${i + 1}/1980x1080">
-        </div>`;
-
-        slider.insertAdjacentHTML("beforeend", render);
+const render = {
+      addImages(){
+        for (let i = 0; i < images; i++) {
+          const render =
+          `<div class="pics fl">
+            <img src="https://source.unsplash.com/random?sig=${i}" alt="pic${i + 1}/900x500">
+          </div>`;
+          slider.insertAdjacentHTML("beforeend", render);
+        }
+        pics.push(...app.querySelectorAll(".pics"));
+      },
+      addDots(){
+        for (let i = 0; i < images; i++) {
+          const render =
+          `<div class="dot" data-pos="${i}">
+            <i class="fa fa-stop" aria-hidden="true"></i>
+          </div>`;
+          dots.insertAdjacentHTML("beforeend", render);
+        }
+        dot.push(...app.querySelectorAll(".dot"));
+        dot.forEach(e => e.addEventListener("click", setDot.select));
       }
-      pics.push(...app.querySelectorAll(".pics"));
 };
+
 
 const slide = {
       next: () => {
         if (count === images - 1){
           count = 0;
-          pics.forEach(i => i.style.transform = `translateX(0)`);
+          pics.forEach(i => i.style.transform = `translateX(${count})`);
         }
         else {
           count++;
           pics.forEach(i => i.style.transform = `translateX(${-100 * count}%)`);
         }
+        setDot.paint();
       },
       prev: () => {
         if (count === 0) {
@@ -38,12 +54,41 @@ const slide = {
           count--;
           pics.forEach(i => i.style.transform = `translateX(${-100 * count}%)`);
         }
+        setDot.paint();
+      },
+      jump: (val) => {
+        pics.forEach(i => i.style.transform = `translateX(${-100 * val}%)`);
+        count = val;
       }
 };
+
+const setDot = {
+      select: function() {
+        let val = this.attributes["data-pos"].value;
+        slide.jump(val);
+        setDot.paint();
+      },
+      paint: () => {
+        const on = app.querySelector(`.dot[data-pos="${count}"]`);
+        const off = app.querySelectorAll(`.dot:not([data-pos="${count}"])`);
+        on.classList.add("dot-on");
+        off.forEach(i => i.classList.remove("dot-on"));
+
+      }
+};
+
+
+const init = () => {
+      render.addImages();
+      render.addDots();
+      setDot.paint();
+};
+
+
 
 next.addEventListener('click', slide.next);
 prev.addEventListener('click', slide.prev);
 
-const interval = setInterval(slide.next, 10000);
+// const interval = setInterval(slide.next, 10000);
 
-window.onload = addImages;
+window.onload = init;
